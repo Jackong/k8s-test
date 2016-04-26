5f2e818a8d0c5a59ef21afe9a6ce1f50fcef3eeb
Jackong <daisy@Jackong.local>
2016-04-26 18:20:52 +0800

    fix master for mount /mnt

--- master.sh
+++ master.sh
@@ -4,6 +4,7 @@ docker run \
     --name=k8s-master \
     --volume=/:/rootfs:ro \
     --volume=/sys:/sys:ro \
+    --volume=/mnt:/mnt:rw \
     --volume=/var/lib/docker/:/var/lib/docker:rw \
     --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
     --volume=/var/run:/var/run:rw \
@@ -11,13 +12,14 @@ docker run \
     --pid=host \
     --privileged=true \
     -d \
-    gcr.io/google_containers/hyperkube-amd64:v1.2.0 \
+    gcr.io/google_containers/hyperkube-amd64:v1.2.2 \
     /hyperkube kubelet \
         --containerized \
-        --hostname-override="127.0.0.1" \
+        --hostname-override="192.168.99.100" \
         --address="0.0.0.0" \
         --api-servers=http://localhost:8080 \
         --config=/etc/kubernetes/manifests \
         --cluster-dns=10.0.0.10 \
         --cluster-domain=cluster.local \
-        --allow-privileged=true --v=2
+        --allow-privileged=true --v=2 \
+	--cadvisor-port=4194 
